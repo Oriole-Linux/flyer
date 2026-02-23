86502f6740d6dfbdc8a061ae592a2a47219887ca
George <coding@gentoo.localhost>
2026-02-23 18:21:02 +0100

    version 0.1.1.0: info command

--- Sources/flyer/Info.swift
+++ Sources/flyer/Info.swift
@@ -20,11 +20,23 @@ struct Info: AsyncParsableCommand {
             return
         }
 
-        let pkg = try decode(from: URL(fileURLWithPath: path))
+        let pkg = try decode(from: URL(fileURLWithPath: build))
 
         print("package \(Colored.blue)\(package)\(Colored.reset)")
-        print("    Name: \(Colored.yellow)\(pkg.name)\(Colored.reset) (in category: \(Colored.yellow))")
-        print("    Version: \(Colored.yellow)\(pkg.version)\(Colored.yellow)")
-        print("    Maintainer: \(Colored.yellow)")
+        print("    Name: \(Colored.yellow)\(pkg.name)\(Colored.reset) (in category: \(Colored.yellow)\(pkg.category)\(Colored.reset))")
+        print("    Version: \(Colored.yellow)\(pkg.version)\(Colored.reset)")
+        print("    Maintainer: \(Colored.yellow)\(pkg.maintainer)\(Colored.reset)")
+        print("    Dependencies: \(Colored.yellow)\(pkg.deps)\(Colored.reset)")
+        if file.fileExists(atPath: "/var/db/flyer/packages/\(pkg.category)/\(pkg.name)") {
+            print("    Installed: \(Colored.green)yes\(Colored.reset)")
+        } else {
+            print("    Installed: \(Colored.red)no\(Colored.reset)")
+        }
+        if verbose {
+            print("    Source: \(Colored.yellow)\(pkg.source)\(Colored.reset)")
+            print("    Configuring command: \(Colored.yellow)\(pkg.configuring)\(Colored.reset)")
+            print("    Compiling command: \(Colored.yellow)\(pkg.build)\(Colored.reset)")
+            print("    Installing command: \(Colored.yellow)\(pkg.install)\(Colored.reset)")
+        }
     }
 }
