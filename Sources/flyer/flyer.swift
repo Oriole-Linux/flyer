d2d26f9d1d9e9311f7094be30e12c7b6dfc33264
George <coding@gentoo.localhost>
2026-02-27 17:03:12 +0100

    ver 0.1.2.1

--- Sources/flyer/flyer.swift
+++ Sources/flyer/flyer.swift
@@ -6,7 +6,7 @@ struct flyer: AsyncParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "flyer",
         abstract: "Source-based package manager in Swift",
-        version: "0.1.2.0",
+        version: "0.1.2.1",
         subcommands: [Install.self, Remove.self, Sync.self, Info.self]
     )
 }
